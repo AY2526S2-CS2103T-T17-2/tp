b7d9cdfab4ec7a4749d4ba82f8b410803179d608
Yong Ee <yongee2003@gmail.com>
2026-04-07 03:16:49 +0800

    Update app ui

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -1,13 +1,13 @@
 package seedu.address.ui;
 
-import java.util.logging.Logger;
-
+import javafx.beans.binding.Bindings;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.Control;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
-import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
 
 /**
@@ -15,7 +15,7 @@ import seedu.address.model.person.Person;
  */
 public class PersonListPanel extends UiPart<Region> {
     private static final String FXML = "PersonListPanel.fxml";
-    private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
+    static final double LIST_VIEW_WIDTH_ALLOWANCE = 18; // Accounts for the list view scrollbar width.
 
     @FXML
     private ListView<Person> personListView;
@@ -25,6 +25,8 @@ public class PersonListPanel extends UiPart<Region> {
      */
     public PersonListPanel(ObservableList<Person> personList) {
         super(FXML);
+        personListView.setMinWidth(0);
+        personListView.setMaxWidth(Double.MAX_VALUE);
         personListView.setItems(personList);
         personListView.setCellFactory(listView -> new PersonListViewCell());
     }
@@ -33,16 +35,54 @@ public class PersonListPanel extends UiPart<Region> {
      * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
      */
     class PersonListViewCell extends ListCell<Person> {
+        private PersonCard personCard;
+
+        PersonListViewCell() {
+            setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+            setMinWidth(0);
+            setMaxWidth(Control.USE_PREF_SIZE);
+        }
+
         @Override
         protected void updateItem(Person person, boolean empty) {
             super.updateItem(person, empty);
+
+            if (getGraphic() instanceof Region currentCard) {
+                currentCard.prefWidthProperty().unbind();
+            }
 
             if (empty || person == null) {
                 setGraphic(null);
                 setText(null);
             } else {
-                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
+                if (personCard == null) {
+                    personCard = new PersonCard(person, getIndex() + 1);
+                } else {
+                    personCard.update(person, getIndex() + 1);
+                }
+
+                Region cardRoot = personCard.getRoot();
+                cardRoot.setMinWidth(0);
+                cardRoot.setMaxWidth(Double.MAX_VALUE);
+                cardRoot.prefWidthProperty().bind(Bindings.max(0,
+                        getListView().widthProperty().subtract(LIST_VIEW_WIDTH_ALLOWANCE)));
+                setGraphic(cardRoot);
+                setText(null);
             }
+        }
+
+        @Override
+        protected double computePrefHeight(double width) {
+            if (getGraphic() instanceof Region cardRoot) {
+                double contentWidth;
+                if (width < 0) {
+                    contentWidth = Math.max(0, cardRoot.prefWidth(-1));
+                } else {
+                    contentWidth = Math.max(0, width - snappedLeftInset() - snappedRightInset());
+                }
+                return cardRoot.prefHeight(contentWidth) + snappedTopInset() + snappedBottomInset();
+            }
+            return super.computePrefHeight(width);
         }
     }
 
